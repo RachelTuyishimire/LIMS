426c8ad5c87fbed6eeb9e9caa98f7c597505768f
student <you@example.com>
2023-06-13 07:31:12 +0300

    Added a chat room

--- script.js
+++ script.js
@@ -57,3 +57,89 @@ const displayExperts = async()=>{
     })
 }
 displayExperts();
+
+
+
+
+// Simulated data for messages and user list
+const messages = [
+    { content: 'Hello!', timestamp: '9:00 AM' },
+    { content: 'How are you?', timestamp: '9:02 AM' },
+    { content: 'Im good, thanks!', timestamp: '9:05 AM' },
+  ];
+  
+  const userList = ['User1', 'User2', 'User3'];
+  
+  // Display messages in the chat window
+  const chatMessages = document.getElementById('chat-messages');
+  messages.forEach((message) => {
+    const content = document.createElement('p');
+    content.textContent = message.content;
+  
+    const timestamp = document.createElement('span');
+    timestamp.textContent = message.timestamp;
+  
+    const messageContainer = document.createElement('div');
+    messageContainer.appendChild(content);
+    messageContainer.appendChild(timestamp);
+  
+    chatMessages.appendChild(messageContainer);
+  });
+  
+  // Display user list
+  const userContainer = document.getElementById('user-list');
+  userList.forEach((user) => {
+    const userElement = document.createElement('p');
+    userElement.textContent = user;
+  
+    userContainer.appendChild(userElement);
+  });
+  
+  // Submit message
+  const chatForm = document.getElementById('chat-form');
+  const messageInput = document.getElementById('message-input');
+  
+  chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const content = messageInput.value;
+    const contentElement = document.createElement('p');
+    contentElement.textContent = content;
+  
+    const timestamp = document.createElement('span');
+    timestamp.textContent = new Date().toLocaleTimeString();
+  
+    const messageContainer = document.createElement('div');
+    messageContainer.appendChild(contentElement);
+    messageContainer.appendChild(timestamp);
+  
+    chatMessages.appendChild(messageContainer);
+  
+    messageInput.value = '';
+  });
+  
+
+
+// $(document).ready(function() 
+//     // Load messages on page load
+//     $.get('/get_messages/', function(data) {
+//         for (var i = 0; i < data.messages.length; i++) {
+//             var message = data.messages[i];
+//             var content = $('<p>').text(message.content);
+//             var timestamp = $('<span>').text(message.timestamp);
+//             $('#chat-messages').append(content).append(timestamp);
+//         }
+//     });
+
+//     $('#chat-form').submit(function(e) {
+//         e.preventDefault();
+//         var content = $('#message-input').val();
+//         $.post('/post_message/', {content: content}, function(data) {
+//             if (data.success) {
+//                 var content = $('<p>').text(content);
+//                 var timestamp = $('<span>').text(new Date().toISOString());
+//                 $('#chat-messages').append(content).append(timestamp);
+//                 $('#message-input').val('');
+//             }
+//         });
+//     });
+// });
